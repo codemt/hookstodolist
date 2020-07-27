1b9eda34d381fe9ae091322294b359729fee8c08
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2020-07-27 23:05:54 +0530

    successfully added delete functionality

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -2,7 +2,7 @@ import React,{ useState, useEffect } from 'react'
 import './index.css'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-export default function Display() {
+export default function Display(props) {
 
     const [todos,fetchTodos] = useState(null);
     useEffect(()=>{
@@ -22,6 +22,37 @@ export default function Display() {
 
     },[])
 
+   
+
+    const deleteTodo = (e) => {
+        e.preventDefault()
+        const id = e.target.id;
+       const deleteTodo = async() => {
+
+            await axios.delete(`https://todolist-auth-express-server.herokuapp.com/api/todo/${id}`)
+            .then(res =>{
+
+                console.log(res.data)
+
+                setTimeout(() =>{
+
+                  //  props.history.push('/')
+                 window.location.reload('/')
+
+                },500)
+
+            })
+            .catch(err =>{
+
+                console.log(err.messege);
+
+            })
+
+        }
+        deleteTodo();
+
+    }
+
     const result = todos;
     console.log(result);
 
@@ -34,12 +65,13 @@ export default function Display() {
                     
                         result.map((todo)=>(
                             
-                        
-                  <Link to={`edit/${todo.id}`}> 
-                    <li class="p-4"> {todo.todo} </li> 
-                 </Link>  
-                     
-
+                    <div class="display container mx-auto flex"> 
+                        <Link to={`edit/${todo.id}`}> 
+                            <li class="p-4"> {todo.todo} </li> 
+                        </Link>  
+                        <button id={todo.id} onClick={deleteTodo} class="p-4"> - </button>
+                    </div>          
+                    
                         ))
                     
                     : <p> Loading... </p>
